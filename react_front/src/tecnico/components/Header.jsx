6d4f137658c5d7d0bd24a243f7c53f5a3e49509c
Wilver <wilverflores2004@gmail.com>
2026-07-25 21:11:32 -0600

    implementacion de iconos en el header

--- react_front/src/tecnico/components/Header.jsx
+++ react_front/src/tecnico/components/Header.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logoIcon from "../icons/i_logo.png";
+import mensajeIcon from "../icons/i_mensaje.png";
+import perfilIcon from "../icons/i_perfil.png";
 
-const Header = ({ user }) => {
+const Header = ({ user, onProfileClick }) => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>SoportITO</div>
-      <input type="text" placeholder="Buscar..." className={styles.search} />
+      {/* Logo */}
+      <div className={styles.logoContainer}>
+        <img src={logoIcon} alt="Logo" className={styles.logoIcon} />
+      </div>
+
+      {/* Barra de búsqueda */}
+      <div className={styles.searchContainer}>
+        <span className={styles.searchLabel}>Buscar</span>
+        <input type="text" placeholder="Buscar..." className={styles.search} />
+      </div>
+
+      {/* Perfil y acciones */}
       <div className={styles.profile}>
-        <button>💬</button>
-        <span>{user?.nombre || "Técnico"}</span>
-        {user?.avatar ? (
-          <img src={user.avatar} alt="Perfil" className={styles.avatar} />
-        ) : (
-          <div className={styles.avatar}>👤</div>
-        )}
+        <button className={styles.iconButton}>
+          <img src={mensajeIcon} alt="Mensajes" className={styles.iconMessage} />
+        </button>
+        <span className={styles.username}>{user?.nombre || "Técnico"}</span>
+        <button className={styles.avatarButton} onClick={onProfileClick}>
+          {user?.avatar ? (
+            <img src={user.avatar} alt="Perfil" className={styles.avatar} />
+          ) : (
+            <img src={perfilIcon} alt="Perfil por defecto" className={styles.avatar} />
+          )}
+        </button>
       </div>
     </header>
   );
