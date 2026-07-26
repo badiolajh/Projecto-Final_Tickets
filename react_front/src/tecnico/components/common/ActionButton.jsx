76b557e40a3300297c7ddaf574f5a74743ccf8d7
Wilver <wilverflores2004@gmail.com>
2026-07-26 16:00:12 -0600

    ajuste del icono y tamño

--- react_front/src/tecnico/components/common/ActionButton.jsx
+++ react_front/src/tecnico/components/common/ActionButton.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import styles from "./ActionButton.module.css";
+import iconVer from "../../icons/i_ver_general.png";
+import iconFinalizar from "../../icons/i_finalizar.png";
+import iconDiagnostico from "../../icons/i_diagnostico.png";
+import iconNuevo from "../../icons/i_mas.png";
 
 const ActionButton = ({ tipo, onClick }) => {
   let label = "";
   let className = "";
+  let icon = null;
 
   switch (tipo) {
     case "ver":
       label = "Ver";
       className = styles.ver;
+      icon = iconVer;
       break;
     case "finalizar":
       label = "Finalizar";
       className = styles.finalizar;
+      icon = iconFinalizar;
       break;
     case "diagnostico":
       label = "Diagnóstico";
       className = styles.diagnostico;
+      icon = iconDiagnostico;
       break;
     case "nuevo":
-      label = "+ Nueva solicitud";
+      label = "(+) Nueva Solicitud de Red";
       className = styles.nuevo;
+      icon = iconNuevo;
       break;
     default:
       label = "Acción";
@@ -29,6 +38,7 @@ const ActionButton = ({ tipo, onClick }) => {
 
   return (
     <button className={`${styles.button} ${className}`} onClick={onClick}>
+      {icon && <img src={icon} alt={label} className={styles.icon} />}
       {label}
     </button>
   );
