2d83f793405c8996e622dd13e70367203b9c6023
Wilver <wilverflores2004@gmail.com>
2026-07-26 22:50:10 -0600

    Nota para quitar elemento de desarrollo

--- react_front/src/empleado/components/grid/MainGeneral.jsx
+++ react_front/src/empleado/components/grid/MainGeneral.jsx
@@ -4,6 +4,7 @@ import styles from "./MainGeneral.module.css";
 const MainGeneral = ({ titulo, children }) => {
   return (
     <main className={styles.main}>
+        {/* Eliminar el titulo cuando se implemente */}
       <h2 className={styles.title}>{titulo}</h2>
       <div className={styles.content}>{children}</div>
     </main>
