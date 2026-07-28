8adb9ef2a110a10e96c4830bcd600ba7d2227f4c
Wilver <wilverflores2004@gmail.com>
2026-07-28 08:22:33 -0600

    ajuste de distibucion del model

--- react_front/src/tecnico/modals/Ver-ticket-pendiente/Ver-pendiente.jsx
+++ react_front/src/tecnico/modals/Ver-ticket-pendiente/Ver-pendiente.jsx
@@ -1,47 +1,58 @@
 import { IoClose } from "react-icons/io5";
 import "./Ver-pendiente.css";
 
-const Tick_Pendiente = ({ isOpen, onClose , user}) => {
+const Tick_Pendiente = ({ isOpen, onClose , user }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <div className="section-title-superior">Ticket de <span>{user ? user.username : "Invitad@"}</span></div>
+        <div className="section-title-superior">
+          Ticket de <span>{user ? user.username : "Invitad@"}</span>
+        </div>
 
         <div className="section-title">Descripción</div>
-        <div className='tarjeta-cuerpo-formulario'>
-          <div className="form-row"><label>Area:</label> <input type="text" placeholder="Recursos Humanos" disabled/></div>
-          <div className="form-row"><label>Puesto:</label> <input type="text" placeholder="Jefe de departamento" disabled/></div>
-          <div className="form-row"><label>Num.ext:</label> <input type="text" placeholder="0325" disabled/></div>
+        <div className="tarjeta-cuerpo-formulario">
+          <div className="form-row">
+            <label>Area:</label> 
+            <input type="text" placeholder="Recursos Humanos" disabled />
+          </div>
+          <div className="form-row">
+            <label>Puesto:</label> 
+            <input type="text" placeholder="Jefe de departamento" disabled />
+          </div>
+          <div className="form-row">
+            <label>Num.ext:</label> 
+            <input type="text" placeholder="0325" disabled />
+          </div>
 
           <div className="form-row"><label>Detalle:</label></div>
           <div className="form-row-columna">
-                      <textarea
-                      disabled
-                        rows="4"
-                        value="Cuento con fallas en mi computadora, es urgente!!!!!"
-                      />
-                    </div>
+            <textarea
+              disabled
+              rows="4"
+              value="Cuento con fallas en mi computadora, es urgente!!!!!"
+            />
+          </div>
         </div>
 
         <div className="modal-actions-etiqueta">
           <label className="etiqueta-asignar">Asignar:</label>
         </div>
         <div className="modal-actions">
-          <button className="btn-cancel" onClick={onClose}><IoClose /> Cerrar</button>
+          <button className="btn-cancel" onClick={onClose}>
+            <IoClose /> Cerrar
+          </button>
           <div className="asignar-container">
-                      <select className="select-asignar">
-                        <option value="tecnico">Tecnico</option>
-                        <option value="admin">Administrador</option>
-                        <option value="empleado">Empleado</option>
-                      </select>
-                    </div>
+            <select className="select-asignar" disabled>
+              <option value="tecnico">Técnico</option>
+              <option value="admin">Administrador</option>
+              <option value="empleado">Empleado</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
-
-
   );
 };
 
