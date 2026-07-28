68adc43418147422a212400a0cba88307eb22c29
Wilver <wilverflores2004@gmail.com>
2026-07-28 02:12:02 -0600

    estilos del boton cerrar

--- react_front/src/empleado/modals/buttons/BtnCerrar.jsx
+++ react_front/src/empleado/modals/buttons/BtnCerrar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./BtnCerrar.module.css";
 
 const BtnCerrar = ({ onClick }) => (
-  <button className={styles.cerrar} onClick={onClick}>
-    X Cerrar
+  <button className={`${styles.button} ${styles.cerrar}`} onClick={onClick}>
+    ✗ Cerrar
   </button>
 );
 
