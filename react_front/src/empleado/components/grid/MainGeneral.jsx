609154a3ccc25585aff764f294610cc92f054328
Wilver <wilverflores2004@gmail.com>
2026-07-28 02:46:04 -0600

    Titulo del navbar eliminado

--- react_front/src/empleado/components/grid/MainGeneral.jsx
+++ react_front/src/empleado/components/grid/MainGeneral.jsx
@@ -3,9 +3,7 @@ import styles from "./MainGeneral.module.css";
 
 const MainGeneral = ({ titulo, children }) => {
   return (
-    <main className={styles.main}>
-        {/* Eliminar el titulo cuando se implemente */}
-      <h2 className={styles.title}>{titulo}</h2>
+    <main className={styles.main}>      
       <div className={styles.content}>{children}</div>
     </main>
   );
