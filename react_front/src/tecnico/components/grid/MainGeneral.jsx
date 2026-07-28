963cd763e249411edb4be2d018c729d9b5a3b336
Wilver <wilverflores2004@gmail.com>
2026-07-28 02:46:32 -0600

    titulo del navbar borrado

--- react_front/src/tecnico/components/grid/MainGeneral.jsx
+++ react_front/src/tecnico/components/grid/MainGeneral.jsx
@@ -4,7 +4,6 @@ import styles from "./MainGeneral.module.css";
 const MainGeneral = ({ titulo, children }) => {
   return (
     <main className={styles.main}>
-      <h2 className={styles.title}>{titulo}</h2>
       <div className={styles.content}>{children}</div>
     </main>
   );
